7cfa8afd0e148b863c0a008ebda2650c4b4fa15d
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-04-07 11:52:13 -0700

    Update app
    Adding webcam video (in addition to screen recording) with the
    same recording start/stop buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,84 @@
 import { useState, useRef, useEffect } from "react";
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const startCapture = () => {
-    console.log("Starting capture...");
-    const video = videoRef.current;
+  const screenVideoRef = useRef<HTMLVideoElement>(null);
+  const startScreenCapture = () => {
+    const video = screenVideoRef.current;
     if (!video) return;
-    const constraints = {
-      video: { 
-        mediaSource: "screen",
-      },
-      audio: false,
-    };
-    console.log("Using constraints", constraints);
     navigator.mediaDevices.getDisplayMedia({video: true, audio: false})
-      .then(s => {
-        video.srcObject = s;
-        console.log("Video source set.");
-      })
+      .then(s => video.srcObject = s)
       .catch(err => console.error(err));
   };
-  const stopCapture = () => {
-    const video = videoRef.current;
+  const stopScreenCapture = () => {
+    const video = screenVideoRef.current;
     if (!video) return;
     (video.srcObject as MediaStream)?.getTracks().forEach(track => track.stop());
     video.srcObject = null;
   };
+
+
+  const camVideoRef = useRef<HTMLVideoElement>(null);
+  const startCamCapture = () => {
+    const video = camVideoRef.current;
+    if (!video) return;
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+      .then(s => video.srcObject = s)
+      .catch(err => console.error(err));
+  };
+  const stopCamCapture = () => {
+    const video = camVideoRef.current;
+    if (!video) return;
+    (video.srcObject as MediaStream)?.getTracks().forEach(track => track.stop());
+    video.srcObject = null;
+  };
+
+
+
   return (
-    <div className="container" style={{
-      display: "flex",
-      flexDirection: "column",
-    }}>
-      <video 
-        ref={videoRef}
-        autoPlay
-        width={1920/4}
-        height={1080/4}
-        style={{
-          border: "1px solid black",
-        }}
-      />
-      <div style={{display: "flex"}}>
-        <button onClick={startCapture}>
-          Start
-        </button>
-        <button onClick={stopCapture}>
-          Stop
-        </button>
+    <div style={{display: "flex"}}>
+      <div className="container" style={{
+        display: "flex",
+        flexDirection: "column",
+      }}>
+        <video 
+          ref={screenVideoRef}
+          autoPlay
+          width={1920/4}
+          height={1080/4}
+          style={{
+            border: "1px solid black",
+          }}
+        />
+        <div style={{display: "flex"}}>
+          <button onClick={startScreenCapture}>
+            Start
+          </button>
+          <button onClick={stopScreenCapture}>
+            Stop
+          </button>
+        </div>
+      </div>
+      <div className="container" style={{
+        display: "flex",
+        flexDirection: "column",
+      }}>
+        <video 
+          ref={camVideoRef}
+          autoPlay
+          width={1920/4}
+          height={1080/4}
+          style={{
+            border: "1px solid black",
+          }}
+        />
+        <div style={{display: "flex"}}>
+          <button onClick={startCamCapture}>
+            Start
+          </button>
+          <button onClick={stopCamCapture}>
+            Stop
+          </button>
+        </div>
       </div>
     </div>
   );
