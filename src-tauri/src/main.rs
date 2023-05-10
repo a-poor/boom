8fdf6d5bcc415acf1931ac37246dca4ca86854d9
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-05-09 19:55:34 -0700

    Starting rust cfg

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
+struct AppState;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// the payload type must implement `Serialize` and `Clone`.
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
+}
+
 fn main() {
+    let app_state = AppState;
+
     tauri::Builder::default()
+        .setup(|app| {
+            // listen to the `event-name` (emitted on any window)
+            let id = app.listen_global("event-name", |event| {
+                println!("got event-name with payload {:?}", event.payload());
+            });
+            // unlisten to the event using the `id` returned on the `listen_global` function
+            // a `once_global` API is also exposed on the `App` struct
+            app.unlisten(id);
+
+            // emit the `event-name` event to all webview windows on the frontend
+            app.emit_all("event-name", Payload { message: "Tauri is awesome!".into() }).unwrap();
+            Ok(())
+        })
+        .manage(app_state)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
